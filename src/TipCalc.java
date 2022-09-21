9c4a61ad23eadca7fc330f5bac76a6a989b12f6f
Mohsin121243 <mchoudhary4645@bths.edu>
2022-09-21 12:06:07 -0400

    added scanner

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class TipCalc {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.print("How much was the bill");
+
     }
 }
