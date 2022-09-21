d5da2eefca6a2a8b2b83fb2608fbecf19d2aefa7
Mohsin121243 <mchoudhary4645@bths.edu>
2022-09-21 12:29:14 -0400

    added values

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 public class TipCalc {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("How much was the bill");
+        System.out.println("How much was the bill");
+      double totalBill = s.nextDouble();
+        System.out.println("What is the tip percentage in whole numbers?");
+        double tipPercentage = s.nextDouble();
+        System.out.println("What was the total number of people at the party/");
+        int totalPeople = s.nextInt();
+        double totalTip = ((double)tipPercentage * 0.01) * totalBill);
+
 
     }
 }
